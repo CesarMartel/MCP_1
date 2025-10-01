d634bc1d9017cb48187ccb864a8c5f5fd9b1b2ff
Neutrolex <60861853campos@gmail.com>
2025-09-30 20:18:39 -0500

    feet: entendiendo como funciona :v

--- prueba.py
+++ prueba.py
@@ -4,38 +4,31 @@ from vertexai.generative_models import GenerativeModel
 
 # Reemplaza con los datos de tu proyecto de Google Cloud
 # Asegúrate de haber instalado 'google-cloud-aiplatform'
-PROJECT_ID = "stone-poetry-473315-a9"  # <--- ¡IMPORTANTE! Reemplaza esto con tu ID de proyecto real
+PROJECT_ID = "stone-poetry-473315-a9" # <--- ¡IMPORTANTE! Reemplaza esto con tu ID de proyecto real
+LOCATION = "us-central1"
 LOCATION = "us-central1"
 
 vertexai.init(project=PROJECT_ID, location=LOCATION)
 
-# ---
-
 # Paso 2: M (Modelo) - Selección del Cerebro Experto
 modelo = GenerativeModel("gemini-2.5-flash")
 
-# ---
-
 # Paso 3: C (Contexto) - Definición de los Datos
-tema_principal = "Programación"
+tema = "Creacion de Prompts correctos" #colocar un tema cualquiera
 
-contexto_formateado = f"""
-- Tema a Explicar: {tema_principal}
+contexto = f""" 
+quiero hacer prompts para elaborar distintos proyectos lo 
+mas rapido posible.
 """
-# ---
 
 # Paso 4: P (Petición) - Construcción del Prompt
-plantilla_prompt = f"""
-Actúa como un desarrollador senior y explique brevemente sobre {tema_principal}.
+prompt = f"""
+actua como un experto en la elaboracion de prompts, detalla cuales 
+son los pasos para hacer uno, que necesito, etc. 
 """
-
-prompt_final = plantilla_prompt.format(contexto_para_usar=contexto_formateado)
-
-# ---
 
 # Paso 5: Ejecución y Resultado
 # Se envía la petición al modelo.
-respuesta = modelo.generate_content(prompt_final)
+respuesta = modelo.generate_content(contexto)
 
-# Se imprime ÚNICAMENTE la respuesta final de la IA.
 print(respuesta.text)
